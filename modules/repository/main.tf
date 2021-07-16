c7bd5f089b1bad45c81d81bbd989554433ce336a
Zach Mowrey <zbmowrey@gmail.com>
2021-07-16 17:06:27 -0400

    Fix formatting

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -4,7 +4,7 @@
 # both repos have develop, stage, and main branches
 
 provider "github" {
-  token = var.github_token
+  token        = var.github_token
   organization = var.github_org
 }
 
@@ -17,67 +17,67 @@ locals {
 }
 
 resource "github_repository" "repo" {
-  for_each = local.repository_type
-  name = "${var.repository_base_name}-${each.value}"
-  description = "${var.repository_base_name} ${each.value} - managed by Terraform"
-  visibility = "private"
-  has_wiki = false
-  has_downloads = false
-  has_issues = false
-  has_projects = false
+  for_each               = local.repository_type
+  name                   = "${var.repository_base_name}-${each.value}"
+  description            = "${var.repository_base_name} ${each.value} - managed by Terraform"
+  visibility             = "private"
+  has_wiki               = false
+  has_downloads          = false
+  has_issues             = false
+  has_projects           = false
   delete_branch_on_merge = true
-  auto_init = true
-  vulnerability_alerts = true
+  auto_init              = true
+  vulnerability_alerts   = true
 }
 
 # Web Repository Branch Creation & Protection
 
 resource "github_branch" "develop" {
-  for_each = github_repository.repo
+  for_each   = github_repository.repo
   repository = each.value["name"]
-  branch = "develop"
+  branch     = "develop"
 }
 resource "github_branch" "staging" {
-  for_each = github_repository.repo
+  for_each   = github_repository.repo
   repository = each.value["name"]
-  branch = "staging"
+  branch     = "staging"
 }
 
 # Create Github Repo Secrets
 
 resource "github_actions_secret" "aws-key-main" {
-  for_each = github_repository.repo
-  repository = each.value["name"]
-  secret_name = "AWS_KEY_MAIN"
+  for_each        = github_repository.repo
+  repository      = each.value["name"]
+  secret_name     = "AWS_KEY_MAIN"
   plaintext_value = var.aws_key_main
 }
 resource "github_actions_secret" "aws-secret-main" {
-  for_each = github_repository.repo
-  repository = each.value["name"]
-  secret_name = "AWS_SECRET_MAIN"
+  for_each        = github_repository.repo
+  repository      = each.value["name"]
+  secret_name     = "AWS_SECRET_MAIN"
   plaintext_value = var.aws_secret_main
 }
 resource "github_actions_secret" "aws-key-staging" {
-  for_each = github_repository.repo
-  repository = each.value["name"]
-  secret_name = "AWS_KEY_STAGING"
+  for_each        = github_repository.repo
+  repository      = each.value["name"]
+  secret_name     = "AWS_KEY_STAGING"
   plaintext_value = var.aws_key_staging
 }
 resource "github_actions_secret" "aws-secret-staging" {
-  for_each = github_repository.repo
-  repository = each.value["name"]
-  secret_name = "AWS_SECRET_STAGING"
+  for_each        = github_repository.repo
+  repository      = each.value["name"]
+  secret_name     = "AWS_SECRET_STAGING"
   plaintext_value = var.aws_secret_staging
 }
 resource "github_actions_secret" "aws-key-develop" {
-  for_each = github_repository.repo
-  repository = each.value["name"]
-  secret_name = "AWS_KEY_DEVELOP"
+  for_each        = github_repository.repo
+  repository      = each.value["name"]
+  secret_name     = "AWS_KEY_DEVELOP"
   plaintext_value = var.aws_key_develop
 }
 resource "github_actions_secret" "aws-secret-develop" {
-  for_each = github_repository.repo
-  repository = each.value["name"]
-  secret_name = "AWS_SECRET_DEVELOP"
+  for_each        = github_repository.repo
+  repository      = each.value["name"]
+  secret_name     = "AWS_SECRET_DEVELOP"
   plaintext_value = var.aws_secret_develop
 }
