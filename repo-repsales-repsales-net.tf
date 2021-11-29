ec69fd1943e98b4ab1385ec782ed791893d7f9de
Zach Mowrey <zbmowrey@gmail.com>
2021-11-29 13:24:04 -0500

    Move to using lookup() for tfvars.cf_distributions for ease of templating.

--- repo-repsales-repsales-net.tf
+++ repo-repsales-repsales-net.tf
@@ -1,21 +1,31 @@
 module "repo-repsales-repsales-net" {
-  providers               = {
+  providers              = {
     github = github.repsales
   }
-  source                  = "./modules/repository"
-  repository_base_name    = "repsales-net"
-  repository_description  = "REP Sales, Inc. - primary site"
-  repository_types        = ["none"]
-  aws_key_main            = var.aws_key_main
-  aws_secret_main         = var.aws_secret_main
-  aws_key_staging         = var.aws_key_staging
-  aws_secret_staging      = var.aws_secret_staging
-  aws_key_develop         = var.aws_key_develop
-  aws_secret_develop      = var.aws_secret_develop
-  github_token            = var.github_token
-  github_org              = "repsales"
-  cf_distribution_develop = var.cf_distributions["repsales-net"]["develop"]
-  cf_distribution_main    = var.cf_distributions["repsales-net"]["main"]
-  cf_distribution_staging = var.cf_distributions["repsales-net"]["staging"]
-  repository_visibility = "public"
+  source                 = "./modules/repository"
+  github_org             = "repsales"
+  repository_base_name   = "repsales-net"
+  repository_description = "REP Sales, Inc. - primary site"
+  repository_types       = ["none"]
+  aws_key_main           = var.aws_key_main
+  aws_secret_main        = var.aws_secret_main
+  aws_key_staging        = var.aws_key_staging
+  aws_secret_staging     = var.aws_secret_staging
+  aws_key_develop        = var.aws_key_develop
+  aws_secret_develop     = var.aws_secret_develop
+  github_token           = var.github_token
+
+  # Try to find cloudfront distribution IDs for each environment. If not found
+  # for any reason, return "".
+
+  cf_distribution_develop = lookup(lookup(var.cf_distributions, "repsales-net", {
+    repsales-net = { develop = "" }
+  }), "develop", "")
+  cf_distribution_main    = lookup(lookup(var.cf_distributions, "repsales-net", {
+    repsales-net = { develop = "" }
+  }), "main", "")
+  cf_distribution_staging = lookup(lookup(var.cf_distributions, "repsales-net", {
+    repsales-net = { develop = "" }
+  }), "staging", "")
+  repository_visibility   = "public"
 }
