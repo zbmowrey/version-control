9f609f8786913ce8795c65f763eb7df25a14e10d
Zach Mowrey <zbmowrey@gmail.com>
2021-12-18 11:38:44 -0500

    Add role secrets to cloud-admin repo

--- repo-zbmowrey-cloud-admin.tf
+++ repo-zbmowrey-cloud-admin.tf
@@ -10,9 +10,12 @@ module "repo-zbmowrey-cloud-admin" {
   create_develop         = false
   create_staging         = false
   terraform_cloud_token  = var.terraform_cloud_token
-  secrets = {
-    TERRAFORM_CLOUD_TOKEN   = var.terraform_cloud_token
-    AWS_ROOT_KEY            = var.aws_root_key
-    AWS_ROOT_SECRET         = var.aws_root_key
+  secrets                = {
+    TERRAFORM_CLOUD_TOKEN = var.terraform_cloud_token
+    AWS_ROOT_KEY          = var.aws_root_key
+    AWS_ROOT_SECRET       = var.aws_root_key
+    AWS_MAIN_ROLE         = var.main_assume_role
+    AWS_STAGING_ROLE      = var.staging_assume_role
+    AWS_DEVELOP_ROLE      = var.develop_assume_role
   }
 }
